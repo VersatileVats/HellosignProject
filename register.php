9365b20b5d34c4f3a7e4c09385fbe063271e4546
VATS <57909173+VersatileVats@users.noreply.github.com>
2022-09-26 13:15:52 +0530

    Strengthened the validations & error handling

--- register.php
+++ register.php
@@ -30,7 +30,7 @@ require 'utilities/common.php';
                                 <h4 class="text-dark mb-4" style="font-weight: bold">Create an Account!</h4>
                             </div>
                             
-                            <form action="utilities/register_script.php" method="post" class="user" novalidate>
+                            <form action="utilities/register_script.php" method="post" class="user">
                                 <div class="mb-3 btn-user" style="border: black 2px solid">
                                     <select onchange="disableIPField()" id="role" name="role" aria-label="Default select example" style="width: 95%;margin-left: 11px;border: none">
                                         <option>Hospital</option>
@@ -46,28 +46,39 @@ require 'utilities/common.php';
                                     <input class="form-control form-control-user" type="text" id="patientName" placeholder="Patient Name" name="p_name" required>
                                 </div>
                                 
-                                <div class="mb-2"><input class="form-control form-control-user" type="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Email Address" name="h_email" required></div>
+                                <div class="mb-2"><input class="form-control form-control-user" type="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Email Address" name="h_email" required>
+                                    <?php if(isset($_GET['emailError'])) { ?>
+                                        <p class="text-center text-danger"><b>Email address already exists! Sign up with another email</b></p>
+                                    <?php } elseif(isset($_GET['emailError1'])) { ?>
+                                        <p class="text-center text-danger"><b>Not a valid email address</b></p>
+                                    <?php } ?>
+                                </div>
                                 <div class="row mb-2">
                                     <div class="col-sm-6 mb-3 mb-sm-0"><input class="form-control form-control-user" type="password" id="examplePasswordInput" placeholder="Password" name="password" required></div>
                                     <div class="col-sm-6"><input class="form-control form-control-user" type="password" id="exampleRepeatPasswordInput" placeholder="Repeat Password" name="password_repeat" required></div>
+                                    <?php if(isset($_GET['pwdError'])) { ?>
+                                        <p class="text-center text-danger"><b>Not a valid password. Password must contain a minimum of 8 characters, atleast 1 number, 1 letter & 1 special character</b></p>
+                                    <?php } elseif(isset($_GET['pwdError1'])) { ?>
+                                        <p class="text-center text-danger"><b>New password and confirm password doesn't match</b></p>
+                                    <?php } ?>
                                 </div>
                                 
                                 <div id="hospitalFields">
                                 
                                 <div class="row mb-2">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
-                                        <input class="form-control form-control-user" type="number" id="beds" placeholder="Number of beds" name="beds" required min=0>
+                                        <input required class="form-control form-control-user" type="number" id="beds" placeholder="Number of beds" name="beds" min=0>
                                     </div>
                                     <div class="col-sm-6">
-                                        <input class="form-control form-control-user" type="number" id="doctors" placeholder="Number of doctors" name="doctors" required min=0>
+                                        <input required class="form-control form-control-user" type="number" id="doctors" placeholder="Number of doctors" name="doctors" min=0>
                                     </div>
                                 </div>
                                 <div class="row mb-2">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
-                                        <input class="form-control form-control-user" type="number" id="beds" placeholder="Available ICU" name="icu" required min=0>
+                                        <input required class="form-control form-control-user" type="number" id="beds" placeholder="Available ICU" name="icu" min=0>
                                     </div>
                                     <div class="col-sm-6">
-                                        <input class="form-control form-control-user" type="number" id="doctors" placeholder="Oxygen cylinders" name="oxygen" required min=0>
+                                        <input required class="form-control form-control-user" type="number" id="doctors" placeholder="Oxygen cylinders" name="oxygen" min=0>
                                     </div>
                                 </div>
                                 </div>
@@ -76,10 +87,10 @@ require 'utilities/common.php';
                                     
                                     <div class="row mb-2">
                                         <div class="col-sm-6 mb-3 mb-sm-0">
-                                            <input class="form-control form-control-user" type="number" id="age" placeholder="Age" name="age" required min=0>
+                                            <input class="form-control form-control-user" type="number" id="age" placeholder="Age" name="age" required min=0 max=150>
                                         </div>
                                         <div class="col-sm-6">
-                                            <input class="form-control form-control-user" type="text" id="phone" placeholder="Phone Number" name="phone" required min=0 maxlength="10">
+                                            <input class="form-control form-control-user" type="text" id="phone" placeholder="Phone Number" name="phone" required min=0 maxlength=10>
                                         </div>
                                     </div>
                                 </div>
@@ -89,7 +100,10 @@ require 'utilities/common.php';
                                 <hr>
                             </form>
                             
-                            <div class="text-center"><a class="small" href="login.php">Already have an account? Login!</a></div>
+                            <div class="text-center" style="cursor: pointer">
+                                <a class="small" href="login.php">Already have an account? Login!</a>
+                                <a class="small m-2" href="play.php">Homepage</a>
+                            </div>
                         </div>
                     </div>
                 </div>
